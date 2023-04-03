eef121fbd873b9f1b7071d38087eedea4df31041
HareemSaad <hareems20014@outlook.com>
2023-04-03 21:30:36 +0500

    -

--- converter/src/main/java/com/converter/converter.java
+++ converter/src/main/java/com/converter/converter.java
@@ -2,21 +2,30 @@ package com.converter;
 
 public class converter {
     public static String toRoman(int number) {
+        //throw error for numbers > 3999 or < 1
         if (number < 1 || number > 3999) {
             return "Invalid input";
         }
         
-        String[] romanThousands = {"", "M", "MM", "MMM"};
-        String[] romanHundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-        String[] romanTens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-        String[] romanOnes = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        // each character is its unit times the index
+        // hundredCharacters[5] = 5 * 100 = D
+        String[] thousandCharacters = {"", "M", "MM", "MMM"};
+        String[] hundredCharacters = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] tensCharacters = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] onesCharacters = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
         
+        // divide to see how many 1000s are there
         int thousands = number / 1000;
-        int hundreds = (number % 1000) / 100;
-        int tens = (number % 100) / 10;
-        int ones = number % 10;
+        // divide the leftover by 100 to get number of 100s
+        int hundreds = (number - thousands * 1000) / 100;
+        // divide the leftover by 100 to get number of 100s
+        int tens = (number - thousands * 1000 - hundreds * 100) / 10;
+        // divide the leftover by 100 to get number of 100s
+        int ones = (number - thousands * 1000 - hundreds * 100 - tens * 10);
+
+        System.out.println(thousands +  " " + hundreds +  " " + tens +  " " + ones);
         
-        String romanNumeral = romanThousands[thousands] + romanHundreds[hundreds] + romanTens[tens] + romanOnes[ones];
+        String romanNumeral = thousandCharacters[thousands] + hundredCharacters[hundreds] + tensCharacters[tens] + onesCharacters[ones];
         
         return romanNumeral;
     }
